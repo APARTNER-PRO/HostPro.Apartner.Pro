c17ae3a0a99cc13217a7f1647a1bd2950a01e7cf
Roman Matviy <roman@matviy.pp.ua>
2026-06-05 13:18:43 +0300

    feat: implement About, HostingTypes, and Breadcrumbs components, and add internationalized redirect rules for English routes

--- next.config.js
+++ next.config.js
@@ -3,5 +3,19 @@ const nextConfig = {
   // output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   trailingSlash: true,
   images: { unoptimized: true },
+  async redirects() {
+    return [
+      {
+        source: '/en',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/en/:path*',
+        destination: '/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 module.exports = nextConfig
